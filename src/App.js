e9e09019b6928beff9b1c7563fe6587f58ccaaf8
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-01-14 13:53:39 +0800

    Adds UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [interval, setInterval] = useState(10)
+    const handleChange = (event) => {
+        const newVal = event.target.value
+        setInterval(newVal)
+    }
+    return (
+        <div>
+
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Interval</InputLabel>
+                <Select
+                    labelId="duration-select-label"
+                    id="duration-select"
+                    value={interval}
+                    label="Interval"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+
+        </div>
+    );
+
 }
 
 export default App;
