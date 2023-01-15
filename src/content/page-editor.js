3483a848e3448ff406c901f12d2c5e27db09dba0
ry-se <ryans1633@gmail.com>
2023-01-15 10:54:42 +0800

    merge

--- src/content/page-editor.js
+++ src/content/page-editor.js
@@ -93,65 +93,14 @@ browser.runtime.onMessage.addListener(async data => {
     else if (jsondata.trigger === 'skipbreak') {
         document.getElementById("breaks-overlay").remove()
     }
-<<<<<<< HEAD
 });
 
 
-function onBL() {
-    let blueLight = document.createElement('div');
-    blueLight.id = "blueLight-13749"
-    blueLight.style.position = "fixed";
-    blueLight.style.top = "0";
-    blueLight.style.left = "0";
-    blueLight.style.width = "100%";
-    blueLight.style.height = "100%";
-    blueLight.style.backgroundColor = "rgb(232, 216, 189)"
-    blueLight.style.mixBlendMode = "multiply"
-    blueLight.style.pointerEvents = "None"
-    blueLight.style.textAlign = "center"
-    blueLight.style.zIndex = "9999999999"
-    document.body.appendChild(blueLight)
-}
-
-function offBL() {
-    document.getElementById("blueLight-13749").remove()
-}
-
-
-getValueOrCreate("blueLight", false)
-    .then(enableBL => {
-        console.log("enablebl", enableBL)
-        if (enableBL) {
-            onBL()
-        } else {
-            offBL()
-        }
-    })
-
-browser.storage.onChanged.addListener(
-    (update) => {
-        console.log(update);
-
-        if (update.blueLight !== undefined) {
-            if (update.blueLight.newValue) {
-                onBL()
-            } else {
-                offBL()
-            }
-        }
-
-    }
-)
-
 document.addEventListener("focus", () => {
-        console.log("focused")
+    console.log("focused")
 });
 
 document.addEventListener("blur", () => {
-        console.log("blurred")
+    console.log("blurred")
 });
 
-
-=======
-});
->>>>>>> e1a46d7c1b9b06f0a845fdfd676f8d41fc197147
